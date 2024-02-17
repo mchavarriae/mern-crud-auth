7abc3d18448eaac3383f968204fc1bcf2de542f5
Marlon Chavarria <marlonc@tevixmd.com>
2024-02-17 11:19:38 -0600

    Fix updaten tasks

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -4,8 +4,8 @@ export const getTasksRequest = async () => axios.get("/tasks");
 
 export const createTaskRequest = async (task) => axios.post("/tasks", task);
 
-export const updateTaskRequest = async (task) =>
-  axios.put(`/tasks/${task._id}`, task);
+export const updateTaskRequest = async (id, task) =>
+  axios.put(`/tasks/${id}`, task);
 
 export const deleteTaskRequest = async (id) => axios.delete(`/tasks/${id}`);
 
